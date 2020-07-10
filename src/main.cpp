1cc2f7220007fd649827aa80fea8a81bed61934b
Sarthak Tanwani <sarthak.tanwani@beaminfo.com>
2020-07-10 12:34:14 +0530

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+
+void setup()
+{
+  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  Serial.println("LED ON here");
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  Serial.println("LED OFF here");
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+}
